78bcb4257ccef73ec2f758e40165336c0e4e2c9f
ACER-16\apron <apron4x@gmail.com>
2024-03-20 07:08:16 +0300

    Initialized project and added initial primitive objects to the scene

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public float xAngle = 360f;
+    public Vector3 newPos = new Vector3(3,6,0);
+    public float newScale = 2f;
+
+    void Start()
+    {
+        transform.position = newPos;
+        transform.localScale = Vector3.one * newScale;
+    }
+
+    void Update()
+    {
+        Material material = Renderer.material;
+
+        material.color = Random.ColorHSV();
+        transform.Rotate(xAngle * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
