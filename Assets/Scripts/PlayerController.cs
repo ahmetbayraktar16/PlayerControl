fb6cb182dc6420c41b005e32176dd2f741a9c339
ACER-16\apron <apron4x@gmail.com>
2024-03-21 07:58:24 +0300

    Player movement implemented with user input and constraints

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float topBound = 3;
+    private float bottomBound = -7;
+    private Rigidbody playerRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    //Moves the player based on arrow key input
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+    }
+
+    //Prevent the player from leaving the top o bottom of the screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z > topBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, topBound);
+        }
+
+        if (transform.position.z < bottomBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, bottomBound);
+        }
+    }
+}
